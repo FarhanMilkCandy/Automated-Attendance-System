72cd778f10b249f011a99224e02d1970f7c28a42
FarhanMilkCandy <farhanahmedlb@gmail.com>
2023-03-29 00:17:35 +0600

    SMS enabled for all employees

--- Helper/SMSHelper.cs
+++ Helper/SMSHelper.cs
@@ -16,7 +16,7 @@ namespace Automated_Attendance_System.Helper
         private SMSController _smsController = new SMSController();
         private static readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1);
         private SMSDTO _smsDTOs = new SMSDTO();
-        private static List<string> _bssITEmp = new List<string>();
+        //private static List<string> _bssITEmp = new List<string>();
 
         public async Task<SMSDTO> GetDTO(string enrollmentNumber)
         {
@@ -37,10 +37,10 @@ namespace Automated_Attendance_System.Helper
 
             try
             {
-                if (_bssITEmp.Count == 0)
-                {
-                    _bssITEmp = await _smsController.GetBSSITIds();
-                }
+                //if (_bssITEmp.Count == 0)
+                //{
+                //    _bssITEmp = await _smsController.GetBSSITIds();
+                //}
                 HttpWebRequest request;
                 SMSDTO smsObj = await GetDTO(idNumber);
                 string smsBody = $"Your attendance was recorded at {punchTime}.\nRegards,\nBSS";
@@ -53,7 +53,8 @@ namespace Automated_Attendance_System.Helper
                     smsBody = $"We are pleased to notify you that your child ({smsObj.Name}, ID: {idNumber.Substring(idNumber.Length - 4)}) has been marked present today: {DateTime.Now.Date.ToString("dd-MM-yyyy")} at {punchTime}.\n\nRegards,\nBSS.";
                 }
                 bool eligible = _smsController.SMSEligible(idNumber).Result;
-                if (eligible && _bssITEmp.Contains(idNumber))
+                //if (eligible && _bssITEmp.Contains(idNumber))
+                if (eligible && idNumber.StartsWith("2200"))
                 {
                     request = (HttpWebRequest)WebRequest.Create(@"https://powersms.banglaphone.net.bd/httpapi/sendsms?userId=bss1&password=Bss123&smsText=" + smsBody + "&commaSeperatedReceiverNumbers=" + smsObj.PhoneNumber);
 
